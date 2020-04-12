0db748d1d2501bc03929782e4cbb33c49394de4e
Rob Young <rob@robyoung.digital>
2020-04-12 14:46:19 +0100

    Add test showing search ordering
    And how it is not what we need

--- search/src/lib.rs
+++ search/src/lib.rs
@@ -6,7 +6,7 @@ use pyo3::wrap_pyfunction;
 
 use tantivy;
 use tantivy::directory::MmapDirectory;
-use tantivy::schema::STORED;
+use tantivy::schema::{Schema, STORED};
 
 #[pyclass]
 struct Index {
@@ -70,14 +70,18 @@ struct Ingredient {
     slug: String,
 }
 
+fn create_schema() -> Schema {
+    let mut schema_builder = tantivy::schema::Schema::builder();
+    schema_builder.add_text_field("recipe_title", STORED);
+    schema_builder.add_text_field("recipe_slug", STORED);
+    schema_builder.add_facet_field("ingredient_slug");
+    schema_builder.add_text_field("ingredient_name", STORED);
+    schema_builder.build()
+}
+
 #[pyfunction]
 fn create_or_open(path: String) -> PyResult<Index> {
-    let mut schema_builder = tantivy::schema::Schema::builder();
-    let recipe_title = schema_builder.add_text_field("recipe_title", STORED);
-    let recipe_slug = schema_builder.add_text_field("recipe_slug", STORED);
-    let ingredient_slug = schema_builder.add_facet_field("ingredient_slug");
-    let ingredient_name = schema_builder.add_text_field("ingredient_name", STORED);
-    let schema = schema_builder.build();
+    let schema = create_schema();
 
     let path = Path::new(&path);
     if !path.exists() || !path.is_dir() {
@@ -101,10 +105,10 @@ fn create_or_open(path: String) -> PyResult<Index> {
 
     Ok(Index {
         writer,
-        recipe_title,
-        recipe_slug,
-        ingredient_name,
-        ingredient_slug,
+        recipe_title: schema.get_field("recipe_title").unwrap(),
+        recipe_slug: schema.get_field("recipe_slug").unwrap(),
+        ingredient_name: schema.get_field("ingredient_name").unwrap(),
+        ingredient_slug: schema.get_field("ingredient_slug").unwrap(),
     })
 }
 
@@ -120,3 +124,77 @@ fn search(_py: Python, m: &PyModule) -> PyResult<()> {
 
 create_exception!(search, Error, pyo3::exceptions::Exception);
 create_exception!(search, IndexError, Error);
+
+#[cfg(test)]
+mod tests {
+    use tantivy::collector::TopDocs;
+    use tantivy::query::BooleanQuery;
+    use tantivy::schema::*;
+    use tantivy::{doc, Index};
+
+    #[test]
+    fn facet_scoring() {
+        let mut schema_builder = Schema::builder();
+
+        let title = schema_builder.add_text_field("title", TEXT | STORED);
+        let ingredient = schema_builder.add_facet_field("ingredient");
+
+        let schema = schema_builder.build();
+        let index = Index::create_in_ram(schema.clone());
+
+        let mut index_writer = index.writer(30_000_000).unwrap();
+
+        // For convenience, tantivy also comes with a macro to
+        // reduce the boilerplate above.
+        index_writer.add_document(doc!(
+            title => "Fried egg",
+            ingredient => Facet::from("/ingredient/egg"),
+            ingredient => Facet::from("/ingredient/oil"),
+        ));
+        index_writer.add_document(doc!(
+            title => "Scrambled egg",
+            ingredient => Facet::from("/ingredient/egg"),
+            ingredient => Facet::from("/ingredient/butter"),
+            ingredient => Facet::from("/ingredient/milk"),
+            ingredient => Facet::from("/ingredient/salt"),
+        ));
+        index_writer.add_document(doc!(
+            title => "Egg rolls",
+            ingredient => Facet::from("/ingredient/egg"),
+            ingredient => Facet::from("/ingredient/garlic"),
+            ingredient => Facet::from("/ingredient/salt"),
+            ingredient => Facet::from("/ingredient/oil"),
+            ingredient => Facet::from("/ingredient/tortilla-wrap"),
+            ingredient => Facet::from("/ingredient/mushroom"),
+        ));
+        index_writer.commit().unwrap();
+
+        let reader = index.reader().unwrap();
+        let searcher = reader.searcher();
+        {
+            let query = BooleanQuery::new_multiterms_query(vec![
+                Term::from_facet(ingredient, &Facet::from_text("/ingredient/egg")),
+                Term::from_facet(ingredient, &Facet::from_text("/ingredient/oil")),
+                Term::from_facet(ingredient, &Facet::from_text("/ingredient/garlic")),
+                Term::from_facet(ingredient, &Facet::from_text("/ingredient/mushroom")),
+            ]);
+            let top_docs_collector = TopDocs::with_limit(2);
+            let top_docs = searcher.search(&query, &top_docs_collector).unwrap();
+
+            let titles: Vec<String> = top_docs
+                .iter()
+                .map(|(_, doc_id)| {
+                    searcher
+                        .doc(*doc_id)
+                        .unwrap()
+                        .get_first(title)
+                        .unwrap()
+                        .text()
+                        .unwrap()
+                        .to_owned()
+                })
+                .collect();
+            assert_eq!(titles, vec!["Egg rolls", "Fried egg"]);
+        }
+    }
+}
