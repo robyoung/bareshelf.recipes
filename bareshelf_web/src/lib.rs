1b735c51cc8fc0afa4c528655fd54f201f1b5f17
Rob Young <rob@robyoung.digital>
2020-04-19 20:47:40 +0100

    Feature flag template embedding
    Embedding the templates is convenient for deployment but it can make the
    development cycle slower.

--- bareshelf_web/src/lib.rs
+++ bareshelf_web/src/lib.rs
@@ -10,14 +10,21 @@ pub async fn run_server() -> std::io::Result<()> {
     let cookie_key =
         base64::decode(&std::env::var("COOKIE_SECRET").expect("COOKIE_SECRET is required"))
             .expect("COOKIE_SECRET is not valid base64");
-    let mut tera = Tera::new("/dev/null/*").unwrap();
-    tera.add_raw_templates(vec![
-        ("index.html", include_str!("../templates/index.html")),
-        ("base.html", include_str!("../templates/base.html")),
-    ])
-    .unwrap();
-    let searcher =
-        bareshelf::searcher(Path::new("./search-index")).expect("Could not open search index");
+    let tera = if cfg!(feature = "embedded_templates") {
+        let mut tera = Tera::new("/dev/null/*").unwrap();
+        tera.add_raw_templates(vec![
+            ("index.html", include_str!("../templates/index.html")),
+            ("base.html", include_str!("../templates/base.html")),
+        ])
+        .unwrap();
+        tera
+    } else {
+        Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap()
+    };
+    let searcher = bareshelf::searcher(Path::new(
+        &std::env::var("SEARCH_INDEX_PATH").unwrap_or_else(|_| "./search-index".to_string()),
+    ))
+    .expect("Could not open search index");
 
     HttpServer::new(move || {
         let tera = tera.clone();
